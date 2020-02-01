28ef1d6258ab6676849e2e42d03118c137c657c1
Aman Verma <avD.ZeeK@gmail.com>
2020-02-01 13:44:56 -0500

    adding a template

---
+++ template1.py
@@ -0,0 +1,35 @@
+"""
+### using template version 1
+
+PROBLEM-STATEMENT/URL
+
+FREE TEXT
+
+CODE  STRUCTURE AND ALGO
+
+TEST DETAILS
+
+TAKE AWAYS:
+
+"""
+
+
+class Solution(object):
+    def self_not_used(self):
+        pass
+
+    def fname(self, param1, param2):
+        """
+        :type param: datatype
+        :type param2: datatype
+        :rtype: datatype
+        """
+        self.self_not_used()  # NICE HACK! THIS COULD HAVE BEEN A STATIC METHOD
+        # ALGO
+
+
+class Main:
+    if __name__ == "__main__":
+        s = Solution()
+        # CALLER
+
